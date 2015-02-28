b5c4b8a294b0c668bc9baf1cef016a10816a2d84
Justin Aquadro <jaquadro@gmail.com>
2015-02-27 21:54:04 -0500

    Update WAILA  API (internal dev use, it doesn't get packed).

--- src/com/jaquadro/minecraft/storagedrawers/integration/Waila.java
+++ src/com/jaquadro/minecraft/storagedrawers/integration/Waila.java
@@ -10,8 +10,12 @@ import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import mcp.mobius.waila.api.IWailaDataProvider;
 import mcp.mobius.waila.api.IWailaRegistrar;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
 
 import java.util.List;
 
@@ -77,6 +81,11 @@ public class Waila extends IntegrationModule
         @Override
         public List<String> getWailaTail (ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
             return currenttip;
+        }
+
+        @Override
+        public NBTTagCompound getNBTData (EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, int x, int y, int z) {
+            return null;
         }
     }
 }
