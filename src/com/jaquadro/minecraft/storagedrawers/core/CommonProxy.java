53e19e2767e3533f5febf3d3a2a088d36e3737d0
Justin Aquadro <jaquadro@gmail.com>
2015-05-06 23:34:19 -0400

    Broke extraction, so fix it.

--- src/com/jaquadro/minecraft/storagedrawers/core/CommonProxy.java
+++ src/com/jaquadro/minecraft/storagedrawers/core/CommonProxy.java
@@ -1,7 +1,11 @@
 package com.jaquadro.minecraft.storagedrawers.core;
 
+import com.jaquadro.minecraft.storagedrawers.block.tile.TileEntityDrawers;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class CommonProxy
 {
@@ -14,5 +18,19 @@ public class CommonProxy
     public void updatePlayerInventory (EntityPlayer player) {
         if (player instanceof EntityPlayerMP)
             ((EntityPlayerMP) player).sendContainerToPlayer(player.inventoryContainer);
+    }
+
+    @SubscribeEvent
+    public void playerInteracts (PlayerInteractEvent event) {
+        if (event.action == PlayerInteractEvent.Action.LEFT_CLICK_BLOCK && event.entityPlayer.capabilities.isCreativeMode) {
+            TileEntity tile = event.world.getTileEntity(event.pos);
+            if (tile instanceof TileEntityDrawers) {
+                int dir = ((TileEntityDrawers) tile).getDirection();
+                if (dir == event.face.ordinal()) {
+                    event.setCanceled(true);
+                    return;
+                }
+            }
+        }
     }
 }
