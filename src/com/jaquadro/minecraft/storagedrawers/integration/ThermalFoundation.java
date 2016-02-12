6d8d2f53629ab2f65ec185d12039f8f63b6526c4
Justin Aquadro <jaquadro@gmail.com>
2016-02-11 23:45:29 -0500

    Fix thermal foundation integration (support older versions)

--- src/com/jaquadro/minecraft/storagedrawers/integration/ThermalFoundation.java
+++ src/com/jaquadro/minecraft/storagedrawers/integration/ThermalFoundation.java
@@ -1,8 +1,13 @@
 package com.jaquadro.minecraft.storagedrawers.integration;
 
 import com.jaquadro.minecraft.storagedrawers.StorageDrawers;
+import com.jaquadro.minecraft.storagedrawers.core.ModItems;
 import com.jaquadro.minecraft.storagedrawers.integration.thermalfoundation.CoFHSecurityProvider;
 import com.mojang.authlib.GameProfile;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 import java.lang.reflect.Method;
 
@@ -19,7 +24,12 @@ public class ThermalFoundation extends IntegrationModule
 
     @Override
     public void init () throws Throwable {
-        classRegistrySocial = Class.forName("cofh.core.RegistrySocial");
+        try {
+            classRegistrySocial = Class.forName("cofh.core.RegistrySocial");
+        }
+        catch (ClassNotFoundException e) {
+            classRegistrySocial = Class.forName("cofh.core.util.SocialRegistry");
+        }
 
         methodPlayerHasAccess = classRegistrySocial.getDeclaredMethod("playerHasAccess", String.class, GameProfile.class);
     }
@@ -27,6 +37,11 @@ public class ThermalFoundation extends IntegrationModule
     @Override
     public void postInit () {
         StorageDrawers.securityRegistry.registerProvider(new CoFHSecurityProvider(this));
+
+        if (StorageDrawers.config.cache.enablePersonalUpgrades) {
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.personalKey, 1, 1), "xxx", "xyx", "xxx",
+                'x', "nuggetSignalum", 'y', ModItems.personalKey));
+        }
     }
 
     public boolean playerHasAccess (String playerName, GameProfile owner) {
