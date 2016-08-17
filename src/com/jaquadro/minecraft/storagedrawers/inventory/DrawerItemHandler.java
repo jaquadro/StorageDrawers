f4abe1fe16a59ed497d3433f5427ce65d867e19e
Justin Aquadro <jaquadro@gmail.com>
2016-08-17 01:21:19 -0400

    Support ore dictionary conversions with transport systems that try to be smarter than me.

--- src/com/jaquadro/minecraft/storagedrawers/inventory/DrawerItemHandler.java
+++ src/com/jaquadro/minecraft/storagedrawers/inventory/DrawerItemHandler.java
@@ -1,8 +1,10 @@
 package com.jaquadro.minecraft.storagedrawers.inventory;
 
+import com.jaquadro.minecraft.storagedrawers.StorageDrawers;
 import com.jaquadro.minecraft.storagedrawers.api.storage.IDrawer;
 import com.jaquadro.minecraft.storagedrawers.api.storage.IDrawerGroup;
 import com.jaquadro.minecraft.storagedrawers.api.storage.IPriorityGroup;
+import com.jaquadro.minecraft.storagedrawers.api.storage.ISmartGroup;
 import com.jaquadro.minecraft.storagedrawers.api.storage.attribute.IVoidable;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.items.IItemHandler;
@@ -17,11 +19,14 @@ public class DrawerItemHandler implements IItemHandler
 
     @Override
     public int getSlots () {
-        return group.getDrawerCount();
+        return group.getDrawerCount() + 1;
     }
 
     @Override
     public ItemStack getStackInSlot (int slot) {
+        if (slot == fallbackInsertSlot())
+            return null;
+
         if (group instanceof IPriorityGroup) {
             int[] order = ((IPriorityGroup) group).getAccessibleDrawerSlots();
             slot = (slot >= 0 && slot < order.length) ? order[slot] : -1;
@@ -39,11 +44,66 @@ public class DrawerItemHandler implements IItemHandler
 
     @Override
     public ItemStack insertItem (int slot, ItemStack stack, boolean simulate) {
-        if (group instanceof IPriorityGroup) {
-            int[] order = ((IPriorityGroup) group).getAccessibleDrawerSlots();
-            slot = (slot >= 0 && slot < order.length) ? order[slot] : -1;
+        if (slot == fallbackInsertSlot()) {
+            if (StorageDrawers.config.cache.enableItemConversion)
+                return insertItemFullScan(stack, simulate);
+            else
+                return stack;
         }
 
+        int orderedSlot = slot;
+        int prevSlot = slot - 1;
+
+        if (group instanceof IPriorityGroup) {
+            int[] order = ((IPriorityGroup) group).getAccessibleDrawerSlots();
+            orderedSlot = (slot >= 0 && slot < order.length) ? order[slot] : -1;
+            prevSlot = (slot >= 1 && slot < order.length) ? order[slot - 1] : -1;
+        }
+
+        if (StorageDrawers.config.cache.enableItemConversion && group.isDrawerEnabled(orderedSlot)) {
+            IDrawer drawer = group.getDrawer(orderedSlot);
+            if (drawer.isEmpty()) {
+                if (!group.isDrawerEnabled(prevSlot))
+                    return insertItemFullScan(stack, simulate);
+                else {
+                    IDrawer prevDrawer = group.getDrawer(prevSlot);
+                    if (!prevDrawer.isEmpty())
+                        return insertItemFullScan(stack, simulate);
+                }
+            }
+        }
+
+        return insertItemInternal(orderedSlot, stack, simulate);
+    }
+
+    private ItemStack insertItemFullScan (ItemStack stack, boolean simulate) {
+        if (group instanceof ISmartGroup) {
+            for (int i : ((ISmartGroup) group).enumerateDrawersForInsertion(stack, false)) {
+                stack = insertItemInternal(i, stack, simulate);
+                if (stack == null)
+                    break;
+            }
+        }
+        else if (group instanceof IPriorityGroup) {
+            int[] order = ((IPriorityGroup) group).getAccessibleDrawerSlots();
+            for (int i = 0; i < order.length; i++) {
+                stack = insertItemInternal(i, stack, simulate);
+                if (stack == null)
+                    break;
+            }
+        }
+        else {
+            for (int i = 0; i < group.getDrawerCount(); i++) {
+                stack = insertItemInternal(i, stack, simulate);
+                if (stack == null)
+                    break;
+            }
+        }
+
+        return stack;
+    }
+
+    private ItemStack insertItemInternal (int slot, ItemStack stack, boolean simulate) {
         if (!group.isDrawerEnabled(slot))
             return stack;
 
@@ -63,7 +123,7 @@ public class DrawerItemHandler implements IItemHandler
 
         if (!simulate) {
             if (drawer.isEmpty())
-                drawer.setStoredItem(stack, insertCount);
+                drawer.setStoredItemRedir(stack, insertCount);
             else
                 drawer.setStoredItemCount(drawer.getStoredItemCount() + insertCount);
         }
@@ -79,6 +139,9 @@ public class DrawerItemHandler implements IItemHandler
 
     @Override
     public ItemStack extractItem (int slot, int amount, boolean simulate) {
+        if (slot == fallbackInsertSlot())
+            return null;
+
         if (group instanceof IPriorityGroup) {
             int[] order = ((IPriorityGroup) group).getAccessibleDrawerSlots();
             slot = (slot >= 0 && slot < order.length) ? order[slot] : -1;
@@ -99,5 +162,9 @@ public class DrawerItemHandler implements IItemHandler
             drawer.setStoredItemCount(drawer.getStoredItemCount() - returnStack.stackSize);
 
         return returnStack;
+    }
+
+    private int fallbackInsertSlot () {
+        return group.getDrawerCount();
     }
 }
