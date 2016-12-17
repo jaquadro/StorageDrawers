8f7da40e132186dc2e1f42a28664020240a6976f
Justin Aquadro <jaquadro@gmail.com>
2016-12-16 20:14:24 -0500

    Get rid of old pack API and old integration files

--- src/com/jaquadro/minecraft/storagedrawers/api/StorageDrawersApi.java
+++ src/com/jaquadro/minecraft/storagedrawers/api/StorageDrawersApi.java
@@ -7,7 +7,7 @@ public class StorageDrawersApi
 {
     private static IStorageDrawersApi instance;
 
-    public static final String VERSION = "1.7.10-1.2.0";
+    public static final String VERSION = "1.10.2-1.3.0";
 
     /**
      * API entry point.
--- src/com/jaquadro/minecraft/storagedrawers/block/IBlockDestroyHandler.java
+++
@@ -1,9 +0,0 @@
-package com.jaquadro.minecraft.storagedrawers.block;
-
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-
-public interface IBlockDestroyHandler
-{
-    void onBlockDestroyed (World world, BlockPos pos);
-}
