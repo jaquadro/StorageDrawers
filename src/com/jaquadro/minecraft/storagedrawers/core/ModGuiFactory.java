997f767b8ba9070bbec491a30902ff7db071b655
Justin Aquadro <jaquadro@gmail.com>
2017-06-27 00:04:59 -0400

    - Fix Forge interface change

--- src/com/jaquadro/minecraft/storagedrawers/core/ModGuiFactory.java
+++ src/com/jaquadro/minecraft/storagedrawers/core/ModGuiFactory.java
@@ -23,17 +23,7 @@ public class ModGuiFactory implements IModGuiFactory
     }
 
     @Override
-    public Class<? extends GuiScreen> mainConfigGuiClass () {
-        return ModConfigGui.class;
-    }
-
-    @Override
     public Set<RuntimeOptionCategoryElement> runtimeGuiCategories () {
-        return null;
-    }
-
-    @Override
-    public RuntimeOptionGuiHandler getHandlerFor (RuntimeOptionCategoryElement element) {
         return null;
     }
 }
