f531673b2856964cd05ba1d0cdadbc9770d224e0
Justin Aquadro <jaquadro@gmail.com>
2024-08-15 21:58:53 -0400

    port swap updates

--- src/main/java/com/jaquadro/minecraft/storagedrawers/core/CommonEventBusSubscriber.java
+++ src/main/java/com/jaquadro/minecraft/storagedrawers/core/CommonEventBusSubscriber.java
@@ -3,9 +3,12 @@ package com.jaquadro.minecraft.storagedrawers.core;
 import com.jaquadro.minecraft.storagedrawers.StorageDrawers;
 import com.jaquadro.minecraft.storagedrawers.block.BlockDrawers;
 import com.jaquadro.minecraft.storagedrawers.config.ClientConfig;
+import com.jaquadro.minecraft.storagedrawers.inventory.SlotUpgrade;
+import com.jaquadro.minecraft.storagedrawers.item.ItemUpgradeStorage;
 import com.jaquadro.minecraft.storagedrawers.util.WorldUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -13,6 +16,7 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.ItemStackedOnOtherEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import org.jetbrains.annotations.NotNull;
 
@@ -38,6 +42,32 @@ public class CommonEventBusSubscriber {
                     //    event.setCanceled(true);
                     //}
                 }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void itemStackedOn (ItemStackedOnOtherEvent event) {
+        if (!(event.getStackedOnItem().getItem() instanceof ItemUpgradeStorage))
+            return;
+        if (!(event.getCarriedItem().getItem() instanceof ItemUpgradeStorage))
+            return;
+
+        if (event.getSlot() instanceof SlotUpgrade slot) {
+            ItemStack stacked = event.getStackedOnItem();
+            ItemStack carried = event.getCarriedItem();
+            ItemStack slotStack = event.getSlot().getItem();
+
+            // NB: Forge 1.20.1 arguments for stacked and carried items are swapped
+            if (ItemStack.matches(slotStack, carried) && !ItemStack.matches(slotStack, stacked)) {
+                carried = event.getStackedOnItem();
+                stacked = event.getCarriedItem();
+            }
+
+            if (slot.canSwapStack(carried)) {
+                event.getSlot().set(carried);
+                event.getCarriedSlotAccess().set(stacked);
+                event.setCanceled(true);
             }
         }
     }
