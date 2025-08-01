10063dd97fef9c5bd6e2d9e6f546450f5d9c117e
Justin Aquadro <jaquadro@gmail.com>
2025-07-31 22:16:59 -0400

    Frame material config option

--- common/src/main/java/com/jaquadro/minecraft/storagedrawers/block/tile/BlockEntityFramingTable.java
+++ common/src/main/java/com/jaquadro/minecraft/storagedrawers/block/tile/BlockEntityFramingTable.java
@@ -5,6 +5,7 @@ import com.jaquadro.minecraft.storagedrawers.api.framing.IFramedSourceBlock;
 import com.jaquadro.minecraft.storagedrawers.api.framing.IFramedBlock;
 import com.jaquadro.minecraft.storagedrawers.block.tile.tiledata.MaterialData;
 import com.jaquadro.minecraft.storagedrawers.components.item.FrameData;
+import com.jaquadro.minecraft.storagedrawers.config.ModCommonConfig;
 import com.jaquadro.minecraft.storagedrawers.core.ModBlockEntities;
 import com.jaquadro.minecraft.storagedrawers.core.ModContainers;
 import com.jaquadro.minecraft.storagedrawers.core.ModDataComponents;
@@ -134,6 +135,9 @@ public class BlockEntityFramingTable extends BaseBlockEntity implements Nameable
         BlockState state = blockItem.getBlock().defaultBlockState();
         if (state.getBlock().hasDynamicShape())
             return false;
+
+        if (!ModCommonConfig.INSTANCE.GENERAL.restrictFramingMaterials.get())
+            return state.isSolid();
 
         try {
             // Will always throw unless overridden, which usually means it's a block that we don't
